5e97c7e862a5aaed9169b89f23823fe537d518cd
skmoh <122020474+skmoh@users.noreply.github.com>
2024-04-08 12:39:14 +0000

    modifier

--- modifier_article.php
+++ modifier_article.php
@@ -32,7 +32,7 @@ if (isset($_GET['id'])) {
         <div class="section-Affectation my-4" >
         <h2 class='m-2'>Etat du matériel</h2>
         <hr class="border-bottom border-3 border-dark">
-        <span>Etat actuel du matériel</span>
+        <span class="test">Etat actuel du matériel</span>
         <hr class="border-bottom border-3 border-dark">
         <div class="container-fluid">
              <form action="" method="">
@@ -79,7 +79,7 @@ if (isset($_GET['id'])) {
        
     </div>
     <div class="section-Affectation my-4" >
-        <span>Changement d'état du matériel</span>
+        <span class="test">Changement d'état du matériel</span>
         <hr class="border-bottom border-3 border-dark">
         <div class="container-fluid">
              <form action="modifier_article.php" method="POST">
@@ -119,9 +119,10 @@ if (isset($_GET['id'])) {
             <select class="form-select" id="Select" aria-label="Default select example">
   <option disabled  selected >Choisir une Batiment...</option>
   </select>
-            <div class="col-5 d-flex">
-            <span for="" class='form-label mt-2' id='span_text'>Bureau:</span>
+           
             </div>
+           <div class="col-4 d-flex">
+            <span for="" class='form-label mt-2' id='span_text'>Bureau:</span>
             </div>
             <div class="container-fluid">
             <div class="row">
@@ -131,28 +132,102 @@ if (isset($_GET['id'])) {
             <a href=""> 
                 <button class="btn btn-primary">Enregistrer</button>
             </a>
+            <!-- z3 -->
+              <span class="test">Fiche de signification du matériel</span>
+        <hr class="border-bottom border-3 border-dark">
+                    <!-- z3 e1 -->
+        <span>Descreption du matériel</span>
+        <hr class="border-bottom border-3 border-dark">
             <div class="row mt-3 d-flex">
             
             <div class="col-4 d-flex">
-                            <span for="" class='form-label mt-2' id='span_text'>Batiment :</span>
-
-            <select class="form-select" id="Select" aria-label="Default select example">
-  <option  selected >Choisir une Batiment...</option>
- 
-  
-  </select>
+                            <span for="" class='form-label mt-2' id='span_text'>Désignation :</span>
+                            <p>ppp</p>
             </div>
             <div class="col-4 d-flex">
-                            <span for="" class='form-label mt-2' id='span_text'>Etage:</span>
-
-            <select class="form-select" id="Select" aria-label="Default select example">
-  <option disabled  selected >Choisir une Batiment...</option>
-  </select>
-            <div class="col-5 d-flex">
-            <span for="" class='form-label mt-2' id='span_text'>Bureau:</span>
+                            <span for="" class='form-label mt-2' id='span_text'>Marque:</span>
+                            <p>ppp</p>
+           
             </div>
+             <div class="col-4 d-flex">
+            <span for="" class='form-label mt-2' id='span_text'>Modéle :</span>
             </div>
-
+            <div class="row mt-3 d-flex">
+            
+            <div class="col-4 d-flex">
+                            <span for="" class='form-label mt-2' id='span_text'>N d'inventaire :</span>
+                            <p>ppp</p>
+            </div>
+            <div class="col-4 d-flex">
+                            <span for="" class='form-label mt-2' id='span_text'>N de série:</span>
+                            <p>ppp</p>
+            
+            </div>
+            <div class="col-4 d-flex">
+            <span for="" class='form-label mt-2' id='span_text'>Date de fin de garantie :</span>
+            </div>
+            <!-- z3 e2 -->
+            <span>Carctéristique du matériel</span>
+        <hr class="border-bottom border-3 border-dark">
+        <div class="Affiché"><center>
+        <p >Aucun résultat a affiché</p></center>
+</div>
+<span>Information sur l'aquisition</span>
+        <hr class="border-bottom border-3 border-dark">
+        <div class="row mt-3 d-flex">
+            
+            <div class="col-4 d-flex">
+                            <span for="" class='form-label mt-2' id='span_text'>Type d'acquisition  :</span>
+                            <p>ppp</p>
+            </div>
+            <div class="col-4 d-flex">
+                            <span for="" class='form-label mt-2' id='span_text'>Code:</span>
+                            <p>ppp</p>
+            
+            </div>
+            <div class="col-4 d-flex">
+            <span for="" class='form-label mt-2' id='span_text'>Date :</span>
+            </div>
+            <span>Information sur la livraison</span>
+        <hr class="border-bottom border-3 border-dark">
+        <div class="row mt-3 d-flex">
+            
+            <div class="col-4 d-flex">
+                            <span for="" class='form-label mt-2' id='span_text'>Date de livraison  :</span>
+                            <p>ppp</p>
+            </div>
+            <div class="col-4 d-flex">
+                            <span for="" class='form-label mt-2' id='span_text'>N bon de livraison:</span>
+                            <p>ppp</p>
+            
+            </div>
+            <span>Information sur la société</span>
+        <hr class="border-bottom border-3 border-dark">
+        <div class="row mt-3 d-flex">
+            
+            <div class="col-4 d-flex">
+                            <span for="" class='form-label mt-2' id='span_text'>Société  :</span>
+                            <p>ppp</p>
+            </div>
+            <div class="col-4 d-flex">
+                            <span for="" class='form-label mt-2' id='span_text'>Télephone  :</span>
+                            <p>ppp</p>
+            
+            </div>
+            <div class="col-4 d-flex">
+            <span for="" class='form-label mt-2' id='span_text'>Fax :</span>
+            </div>
+            
+            <div class="col-4 d-flex">
+            <span for="" class='form-label mt-2' id='span_text'>Email :</span>
+            </div>
+            
+            <div class="col-4 d-flex">
+            <span for="" class='form-label mt-2' id='span_text'>Adresse :</span>
+            </div>
+            <span class="test">Histoire des états du matériel</span>
+        <hr class="border-bottom border-3 border-dark">
+        <div>Vrsion 0.1</div>
     </div>
     </div>
         </body>
